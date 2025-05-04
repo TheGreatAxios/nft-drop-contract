009903851eb2e85f3950c3a6b921f7bfe70cc224
TheGreatAxios <sawyer@dirtroad.dev>
2025-05-04 08:49:07 -0700

    Add Compiling Contract & Fix Test, Script

---
+++ src/NFTDrop.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import {ERC1155Burnable} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+import {ERC1155Pausable} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Pausable.sol";
+import {ERC1155Supply} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+
+error MaxClaimAmountReached();
+
+contract NFTDrop is ERC1155, AccessControl, ERC1155Pausable, ERC1155Burnable, ERC1155Supply {
+        
+    bytes32 public constant CLAIM_ROLE = keccak256("CLAIM_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant URI_SETTER_ROLE = keccak256("URI_SETTER_ROLE");
+
+    constructor() ERC1155("") {
+        _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _grantRole(PAUSER_ROLE, _msgSender());
+        _grantRole(MINTER_ROLE, _msgSender());
+        _grantRole(MANAGER_ROLE, _msgSender());
+        _setRoleAdmin(CLAIM_ROLE, MANAGER_ROLE); // This makes MANAGER_ROLE admin of CLAIM_ROLE, greater security
+    }
+
+    function claim() external onlyRole(CLAIM_ROLE) {
+        if (balanceOf(_msgSender(), 1) >= 20) revert MaxClaimAmountReached();
+        _mint(_msgSender(), 1, 1, "");
+    }
+
+    function addUserClaim(address user) external onlyRole(MANAGER_ROLE) {
+        grantRole(CLAIM_ROLE, user);
+    }
+
+    function revokeUserClaim(address user) external onlyRole(MANAGER_ROLE) {
+        revokeRole(CLAIM_ROLE, user);
+    }
+
+    function setURI(string memory newuri) public onlyRole(URI_SETTER_ROLE) {
+        _setURI(newuri);
+    }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+        public
+        onlyRole(MINTER_ROLE)
+    {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyRole(MINTER_ROLE)
+    {
+        _mintBatch(to, ids, amounts, data);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256[] memory ids, uint256[] memory values)
+        internal
+        override(ERC1155, ERC1155Pausable, ERC1155Supply)
+    {
+        super._update(from, to, ids, values);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC1155, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
